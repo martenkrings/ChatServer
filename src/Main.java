e51aa197988f5496ea51d6a6629e892840ffcd11
Marten <krings.marten@gmail.com>
2016-11-16 16:01:42 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Marten on 11/16/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main().run(args);
+    }
+
+    private void run(String[] args) {
+
+    }
+}
